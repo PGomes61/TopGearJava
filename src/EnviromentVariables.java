5d3e518ef4def501f4d0afbaa6668b7c386d3f17
Nick <nicolaspinheirogomes3@gmail.com>
2024-08-30 14:21:16 -0300

    não me pergunte oq tá acontecendo...

--- src/EnviromentVariables.java
+++ src/EnviromentVariables.java
@@ -1,27 +1,27 @@
 public class EnviromentVariables{
-    public String SPRITE_C1_E="src/Carro/Carro1E.png";
-    public String SPRITE_C1_F="src/Carro/Carro1F.png";
-    public String SPRITE_C1_D="src/Carro/Carro1D.png";
+    public String SPRITE_C1_E="Carro/Carro1E.png";
+    public String SPRITE_C1_F="Carro/Carro1F.png";
+    public String SPRITE_C1_D="Carro/Carro1D.png";
 
-    public String SPRITE_C2_F="src/Carro/Carro2F.png";
-    public String SPRITE_C2_E="src/Carro/Carro2E.png";
-    public String SPRITE_C2_D="src/Carro/Carro2D.png";
+    public String SPRITE_C2_F="Carro/Carro2F.png";
+    public String SPRITE_C2_E="Carro/Carro2E.png";
+    public String SPRITE_C2_D="Carro/Carro2D.png";
 
-    public String SPRITE_C3_F="src/Carro/Carro3F.png";
-    public String SPRITE_C3_E="src/Carro/Carro3E.png";
-    public String SPRITE_C3_D="src/Carro/Carro3D.png";
+    public String SPRITE_C3_F="Carro/Carro3F.png";
+    public String SPRITE_C3_E="Carro/Carro3E.png";
+    public String SPRITE_C3_D="Carro/Carro3D.png";
 
-    public String SPRITE_C4_F="src/Carro/Carro4F.png";
-    public String SPRITE_C4_E="src/Carro/Carro4E.png";
-    public String SPRITE_C4_D="src/Carro/Carro4D.png";
+    public String SPRITE_C4_F="Carro/Carro4F.png";
+    public String SPRITE_C4_E="Carro/Carro4E.png";
+    public String SPRITE_C4_D="Carro/Carro4D.png";
 
-    public String SPRITE_C5_F="src/Carro/Carro5F.png";
-    public String SPRITE_C5_E="src/Carro/Carro5E.png";
-    public String SPRITE_C5_D="src/Carro/Carro5D.png";
+    public String SPRITE_C5_F="Carro/Carro5F.png";
+    public String SPRITE_C5_E="Carro/Carro5E.png";
+    public String SPRITE_C5_D="Carro/Carro5D.png";
 
-    public String SPRITE_C6_F="src/Carro/Carro6F.png";
-    public String SPRITE_C6_E="src/Carro/Carro6E.png";
-    public String SPRITE_C6_D="src/Carro/Carro6D.png";
+    public String SPRITE_C6_F="Carro/Carro6F.png";
+    public String SPRITE_C6_E="Carro/Carro6E.png";
+    public String SPRITE_C6_D="Carro/Carro6D.png";
     
     public EnviromentVariables(){
 
